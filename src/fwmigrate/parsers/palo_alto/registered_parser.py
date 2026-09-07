1b18f47dc16c8167bc56c1b67ca7cbcc03fbe8f9
CHA ZI YU <23120943@siswa.um.edu.my>
2026-09-07 22:06:47 +0800

    Register PAN-OS IPv6 NAT semantic coverage

--- src/fwmigrate/parsers/palo_alto/registered_parser.py
+++ src/fwmigrate/parsers/palo_alto/registered_parser.py
@@ -5,6 +5,7 @@ from typing import Any, Dict, List, Optional
 
 from fwmigrate.extraction.models import ExtractionStatus
 
+from .ipv6_nat_coverage import PANOSIPv6NATSemanticsCoverageMixin
 from .nat_interface_address_coverage import PANOSNATInterfaceAddressCoverageMixin
 from .policy_nat_coverage import PANOSSourceParser as _CoveragePANOSSourceParser
 from .policy_order import sync_effective_order_to_ir
@@ -13,6 +14,7 @@ from .security_profile_group_safety import PANOSSecurityProfileGroupPolicySafety
 
 
 class PANOSSourceParser(
+    PANOSIPv6NATSemanticsCoverageMixin,
     PANOSNATInterfaceAddressCoverageMixin,
     PANOSSecurityProfileGroupPolicySafetyMixin,
     PANOSSecurityProfileCoverageMixin,
