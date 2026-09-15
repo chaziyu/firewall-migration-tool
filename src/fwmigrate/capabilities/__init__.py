2ba6199933fb1151a8cd68d8a291752f02fcc5bb
CHA ZI YU <23120943@siswa.um.edu.my>
2026-09-15 12:07:34 +0800

    Install FortiGate NAT capability remediation

--- src/fwmigrate/capabilities/__init__.py
+++ src/fwmigrate/capabilities/__init__.py
@@ -7,6 +7,13 @@ from fwmigrate.capabilities.schema import (
     FeatureSupport,
     VendorCapabilityProfile,
 )
+from fwmigrate.capabilities.fortigate_nat_remediation import (
+    install_fortigate_nat_capability_remediation,
+)
+
+install_fortigate_nat_capability_remediation(
+    __import__("fwmigrate.capabilities.analyzer", fromlist=["CapabilityAnalyzer"])
+)
 
 __all__ = [
     "CapabilityAnalyzer",
