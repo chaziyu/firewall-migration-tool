2284ac05f52d72fa5bc0c5d04f0fedb2e7d64b2d
CHA ZI YU <23120943@siswa.um.edu.my>
2026-09-15 16:20:59 +0800

    Reduce test suite to migration safety boundaries

--- tests/core/test_normalizer.py
+++
@@ -1,5 +0,0 @@
-from fwmigrate.core.normalizer import RuleNormalizer
-
-
-def test_normalizer_exposes_outbound_threat_source_normalization():
-    assert hasattr(RuleNormalizer, "normalize_outbound_threat_source_anomalies")
